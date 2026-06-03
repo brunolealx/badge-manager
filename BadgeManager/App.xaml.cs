6d2196d5e71272fc733b4109923d537193fd49ee
Bruno Silva <lealbruno759@gmail.com>
2026-06-02 23:05:56 -0300

    Reorganização do projeto para C# e criação da estrutura base

---
+++ BadgeManager/App.xaml.cs
@@ -0,0 +1,14 @@
+﻿using System.Configuration;
+using System.Data;
+using System.Windows;
+
+namespace BadgeManager
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+    }
+
+}
